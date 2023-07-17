65eb3e69cfc2135d0ab39015f6fbfaa14a211373
Jaime Andres Gomez Gutierrez <jaime.gomez@netto.com.co>
2023-07-17 11:27:23 -0500

    refactor demo component

--- packages/demo-component/src/components/Button/index.tsx
+++ packages/demo-component/src/components/Button/index.tsx
@@ -1,27 +1,5 @@
-import './button.css'
-
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large'
-  /**
-   * Button contents
-   */
-  label: string
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void
-}
+import './styles.css'
+import { ButtonProps } from '../../types/button'
 
 /**
  * Primary UI component for user interaction
